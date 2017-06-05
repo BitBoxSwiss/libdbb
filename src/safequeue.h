5b146eb8735ca22eb8182a58c14381996ebaf173
Jonas Schnelli <dev@jonasschnelli.ch>
2017-06-05 22:27:36 +0200

    Add thread safe queue class

---
+++ src/safequeue.h
@@ -0,0 +1,66 @@
+// Copyright (c) 2017 Shift Devices AG
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef LIBDBB_SAFEQUEUE_H
+#define LIBDBB_SAFEQUEUE_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <atomic>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+// A threadsafe-queue.
+template <class T>
+class SafeQueue
+{
+public:
+  SafeQueue(void)
+    : q()
+    , m()
+    , c()
+  {}
+
+  ~SafeQueue(void)
+  {}
+
+  // Add an element to the queue.
+  size_t size()
+  {
+    std::lock_guard<std::mutex> lock(m);
+    return q.size();
+  }
+
+  // Add an element to the queue.
+  void enqueue(T t)
+  {
+    std::lock_guard<std::mutex> lock(m);
+    q.push(t);
+    c.notify_one();
+  }
+
+  // Get the "front"-element.
+  // If the queue is empty, wait till a element is avaiable.
+  T dequeue(void)
+  {
+    std::unique_lock<std::mutex> lock(m);
+    while(q.empty())
+    {
+      // release lock as long as the wait and reaquire it afterwards.
+      c.wait(lock);
+    }
+    T val = q.front();
+    q.pop();
+    return val;
+  }
+
+private:
+  std::queue<T> q;
+  mutable std::mutex m;
+  std::condition_variable c;
+};
+
+#endif // LIBDBB_SAFEQUEUE_H
